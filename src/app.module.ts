ec1faabedbb902b00bd4096762e65df1850fe97b
Rudra Pramanik <rudrapramanik@Rudras-MacBook-Pro.local>
2024-01-19 02:34:29 +0600

    initial-all

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { TasksController } from './no-spec/tasks/tasks.controller';
+
+@Module({
+  // eslint-disable-next-line prettier/prettier
+  imports: [TasksModule],
+  controllers: [TasksController],
+})
+export class AppModule {}
