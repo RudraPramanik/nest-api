4f9256e3f2637799dc22edd63916de227da00ca3
Rudra Pramanik <rudrapramanik@Rudras-MacBook-Pro.local>
2024-01-19 22:11:42 +0600

    15

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-import { TasksController } from './no-spec/tasks/tasks.controller';
+// import { TasksController } from './no-spec/tasks/tasks.controller';
 
 @Module({
   // eslint-disable-next-line prettier/prettier
   imports: [TasksModule],
-  controllers: [TasksController],
+  // controllers: [TasksController],
 })
 export class AppModule {}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { title } from 'process';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private taskservice: TasksService) {}
+  @Get()
+  getAllTasks(): Task[] {
+    return this.taskservice.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title')title: string,
+    @Body('description')description: string
+  ) : Task {
+    return this.taskservice.createTask(title,description)
+  }
+}
+ 
